c1e95b620b3dd47f81121667cf4db638cd5bb540
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-09-30 09:47:15 +0530

    [COMMIT] Adding minor changes to python ts node js repos. Adding projects for the same

---
+++ typescript/index.ts
@@ -0,0 +1,9 @@
+
+import { prime } from "./src/primes";
+import { isPrimeConventionalWay, isPrimeSquarerootWay, isPrimeSquarerootWayTwo } from "./src/primes_alternate_ways";
+
+
+export { prime as prime };
+export { isPrimeConventionalWay as conventional };
+export { isPrimeSquarerootWay as sqroot };
+export { isPrimeSquarerootWayTwo as sqrootTwo };
