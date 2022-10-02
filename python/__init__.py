30055889eddd826e7fdd933ff4575692a8bee628
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-02 16:59:56 +0530

    [COMMIT] Adding minor changes to deps for cleaning unmanaged deps

--- python/__init__.py
+++ python/__init__.py
@@ -1,8 +1,11 @@
 
 import sys
-sys.path.append("./primes")
+import os
+DIRNAME = os.path.dirname(__file__)
+sys.path.append(DIRNAME)
 
 from primes import primes as fast, primes_alternate_ways as alternate
+
 
 __all__ = ["fast", "alternate"]
 
